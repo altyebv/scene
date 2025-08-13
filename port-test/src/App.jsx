a849ae3d3675b659a85231bdbd6ddea7335111c3
altyebv <altyeb.404@gmail.com>
2025-08-13 21:24:03 +0300

    updated screen

--- port-test/src/App.jsx
+++ port-test/src/App.jsx
@@ -1,14 +1,11 @@
 
 import './App.css'
-// import ChatGPTClone from './components/ChatScreen';
 
 import Scene from './components/Scene';
 import Laptop from './components/Laptop';
-import Desktop from './components/desktop';
 
 function App() {
   // return <Second />;
-  // return <Desktop />;
   return <Laptop />;
 
 }
