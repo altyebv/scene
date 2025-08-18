7162c7a1459610d07aa05ea1500c73b209a14de0
altyebv <altyeb.404@gmail.com>
2025-08-18 21:36:05 +0300

    modify : interface

--- port-test/src/App.jsx
+++ port-test/src/App.jsx
@@ -1,15 +1,19 @@
 
 import './App.css'
 
+
 import Second from './components/Second';
 // import Laptop from './components/Laptop';
-// import Lap from './components/Lap';
-// import Brain from './temp/BrainAnime';
+import Lap from './components/LapScreen';
+import Desktop from './components/desktop';
+// import Desktop from './temp/Desk';
+import Brain from './temp/BrainAnime';
 
 function App() {
-  // return <Desktop />;
+  return <Desktop />;
+  // return <Brain/>
+  // return <Second/>
   // return <Lap/>;
-  return <Second/>
 }
 
 export default App;
