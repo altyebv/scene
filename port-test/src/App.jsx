7d3c8938f75be49d6055fed9fb1649fc0519430f
altyebv <altyeb.404@gmail.com>
2025-08-18 22:03:26 +0300

    configure

--- port-test/src/App.jsx
+++ port-test/src/App.jsx
@@ -2,12 +2,12 @@
 import './App.css'
 
 
-import Second from './components/Second';
+// import Second from './components/Second';
 // import Laptop from './components/Laptop';
-import Lap from './components/LapScreen';
+// import Lap from './components/LapScreen';
 import Desktop from './components/desktop';
 // import Desktop from './temp/Desk';
-import Brain from './temp/BrainAnime';
+// import Brain from './temp/BrainAnime';
 
 function App() {
   return <Desktop />;
