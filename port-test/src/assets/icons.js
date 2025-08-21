fcdb220e6a0353606d66d0d98b8817ed05ab11ea
altyebv <altyeb.404@gmail.com>
2025-08-21 04:21:12 +0300

    added the icons

---
+++ port-test/src/assets/icons.js
@@ -0,0 +1,126 @@
+import Wheather from './desktop_ic/weather.svg';
+import Windows from './desktop_ic/windows.svg';
+import Tasks from './desktop_ic/tasks.svg';
+import Folder from './desktop_ic/folder.svg';
+import Code from './desktop_ic/vscode.svg';
+import Chrome from './desktop_ic/chrome.svg';
+import Teams from './desktop_ic/microsoft-teams.svg';
+import arrow from './desktop_ic/arrowUp.svg';
+import Cloud from './desktop_ic/cloud-off.svg';
+import wifi from './desktop_ic/wifi.svg';
+import sound from './desktop_ic/sound-mute.svg';
+import battery from './desktop_ic/charging.svg';
+import RecycleBin from './desktop_ic/recycle-bin.svg';
+import Github from './desktop_ic/github.svg';
+import Pdf from './desktop_ic/pdf.svg';
+import Wallpaper from './desktop_ic/desktop_wallpaper.jpg';
+import Pycharm from './desktop_ic/pycharm.svg';
+import Chat from './desktop_ic/Assistant.svg';
+import Spotify from './desktop_ic/spotify.svg';
+import arup from './desktop_ic/arUp.svg';
+import arleft from './desktop_ic/arleft.svg';
+import arRight from './desktop_ic/aright.svg';
+import Hdd from './desktop_ic/hdd.svg';
+
+import call from './desktop_ic/phone.svg';
+import video from './desktop_ic/video.svg';
+import group from './desktop_ic/group.svg';
+import calender from './desktop_ic/calender.svg';
+import chat from './desktop_ic/chat_teams.svg';
+import tmSettings from './desktop_ic/setting_team.svg';
+import tmFolder from './desktop_ic/teamspkg.svg';
+import info from './desktop_ic/info.svg';
+import exp from './desktop_ic/exp.svg';
+import skills from './desktop_ic/skills.svg';
+import projects from './desktop_ic/projects.svg';
+import css from './desktop_ic/css.svg'
+import html from './desktop_ic/html.svg'
+import json from './desktop_ic/json.svg'
+import jsx from './desktop_ic/jsx.svg'
+import play from './desktop_ic/Play.svg';
+import pause from './desktop_ic/pause.svg';
+import fav from './desktop_ic/fav.svg';
+import next from './desktop_ic/next.svg';
+import prev from './desktop_ic/prev.svg';
+import shuffleOn from './desktop_ic/shuffleOn.svg';
+import shuffleOff from './desktop_ic/shuffleOff.svg';
+import repeatOn from './desktop_ic/repeatOn.svg';
+import repeatOff from './desktop_ic/repeatOff.svg';
+import devices from './desktop_ic/devices.svg';
+import home from './desktop_ic/home.svg';
+import library from './desktop_ic/library.svg';
+import search from './desktop_ic/search_sptfy.svg';
+import loved from './desktop_ic/loved.svg';
+import music from './desktop_ic/music.svg';
+import soundOn from './desktop_ic/soundOn.svg';
+import edu from './desktop_ic/education.svg';
+
+
+
+
+
+
+
+export const AppsIC = {
+    group,
+    calender,
+    chat,
+    tmSettings,
+    tmFolder,
+    info,
+    exp,
+    skills,
+    projects,
+    css,
+    html,
+    json,
+    jsx,
+    call,
+    video,
+    play,
+    pause,
+    fav,
+    next,
+    prev,
+    home,
+    library,
+    shuffleOn,
+    shuffleOff,
+    repeatOn,
+    repeatOff,
+    devices,
+    search,
+    loved,
+    music,
+    soundOn,
+    edu
+}
+
+
+export const icons = {
+    weather: Wheather,
+    windows: Windows,
+    tasks: Tasks,
+    folder: Folder,
+    code: Code,
+    chrome: Chrome,
+    teams: Teams,
+    arrow: arrow,
+    cloud: Cloud,
+    wifi: wifi,
+    sound: sound,
+    battery: battery,
+    recycle: RecycleBin,
+    github: Github,
+    pdf: Pdf,
+    wallpaper: Wallpaper,
+    pycharm: Pycharm,
+    chat: Chat,
+    spotify: Spotify,
+    hdd: Hdd,
+    arRight: arRight,
+    arleft: arleft,
+    arup: arup,
+    search
+};
+
